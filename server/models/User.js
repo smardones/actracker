2fae004c890dda0e84ad528559b49a5aa2a5f02c
smardone <shaun.mardones@gmail.com>
2020-11-04 21:24:52 -0500

    initial commit

---
+++ server/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: function(em) {
+            return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(em)
+            },
+            message: 'Please provide a valid email address'
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        minlength: 5
+    },
+    islandName: {
+        type: String
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const { appendFileSync } = require('fs');
+const path = require('path');
+// const { ApolloServer } = require('apollo-server');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+const db = require('./config/connection');
+
+// const server = new ApolloServer({
+//     typeDefs,
+//     resolvers,
+//     context: authMiddleware
+//   });
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+    //   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
