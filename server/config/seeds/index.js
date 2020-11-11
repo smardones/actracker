09f1d485926d3ffbabbf80a1719531f15bc30bb2
smardone <shaun.mardones@gmail.com>
2020-11-11 18:49:24 -0500

    added seed script

---
+++ server/config/seeds/index.js
@@ -0,0 +1,39 @@
+const db = require('../connection');
+const mongoose = require('mongoose');
+const {Bug} = require('../../models');
+const fetch = require("node-fetch");
+
+db.once('open', async () => {
+    await Bug.deleteMany({});
+
+    fetch(`http://acnhapi.com/v1/bugs/`)
+    .then(response => response.json())
+    .then((data) => { 
+        const newBugArray = Object.entries(data);
+        newBugArray.forEach((bug) => {
+            const bugDoc = new Bug({
+                name: bug[1].name['name-USen'],
+                availability: {
+                    monthNorthern: bug[1].availability['month-northern'],
+                    monthSouthern: bug[1].availability['month-southern'],
+                    time: bug[1].availability.time,
+                    monthNorthernArray: bug[1].availability['month-array-northern'],
+                    monthSouthernArray: bug[1].availability['month-array-southern'],
+                    timeArray: bug[1].availability['time-array']
+                },
+                price: bug[1].price,
+                priceFlick: bug[1]['price-flick'],
+                description: bug[1]['museum-phrase'],
+                image: bug[1].image_uri,
+                icon: bug[1].icon_uri
+            });
+
+            bugDoc.save();
+        })
+        
+    })
+    .then(() => {
+        return console.log('Seeding Complete')})
+    .catch(err => console.log(err));
+});
+
